2144919ded59f3d575abd8f1bf383275ca33ebf9
duressa-feyissa <duresafeyisa2022@gmail.com>
2023-09-25 23:00:56 +0300

    :fire: Accept Port from env

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from api import app
 import uvicorn
+from api import app
+import os
+
+
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=os.getenv("PORT", 8000))
